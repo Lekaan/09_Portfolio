fca8a8daff0a44f3410f3564e496e3a733035a6b
kaans <kaans@DESKTOP-U4CBDT5>
2016-09-26 13:48:39 +0200

    Add project files.

---
+++ 09_Portfolio/IAsset.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _09_Portfolio
+{
+    internal interface IAsset
+    {
+        double GetValue();
+    }
+    
+}
---
+++ 09_Portfolio/SavingsAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _09_Portfolio
+{
+    internal class SavingsAccount : IAsset
+    {
+        public string Account { get; set; }
+        public double InterestRate { get; set; }
+        public double Value { get; set; }
+
+        public SavingsAccount(string Account, int Value, double InterestRate)
+        {
+            this.Account = Account;
+            this.Value = Value;
+            this.InterestRate = InterestRate;
+        }
+
+        public override string ToString()
+        {
+            string value = Value.ToString("F1").Replace(",", ".");
+            string interestRate = InterestRate.ToString("F1").Replace(",", ".");
+
+            return "SavingsAccount[value=" + value + ",interestRate=" + interestRate + "]";
+        }
+
+        public double GetValue()
+        {
+            return Value;
+        }
+
+        internal void ApplyInterest()
+        {
+            Value = Value*((InterestRate/100)+1);
+        }
+    }
+}
